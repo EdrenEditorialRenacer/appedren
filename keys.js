f71806ec354e6248b385d658fdf6e0189106a806
Dhux93 <mario_mario93@hotmail.com>
2021-03-20 17:30:41 -0600

    configurando bd del hosting

--- keys.js
+++ keys.js
@@ -1,15 +1,15 @@
 module.exports = {
-    // database: {
-    //      host: 'localhost',
-    //      user: 'edrenpublicacion_jose_salazar_garcia',
-    //      password: '~{0E2h{~cq{!',
-    //      database: 'edrenpublicacion_db_edren'
-    //  }
     database: {
         host: 'localhost',
-        user: 'root',
-        password: 'root141512',
-        database: 'database_edren'
+        user: 'edrenpublicacion_jose_salazar_garcia',
+        password: '~{0E2h{~cq{!',
+        database: 'edrenpublicacion_db_edren'
     }
-    
+    // database: {
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: 'root141512',
+    //     database: 'database_edren'
+    // }
+
 };
