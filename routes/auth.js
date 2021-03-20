1dc549f379c496108113b9715d707e0430053e1e
Dhux93 <mario_mario93@hotmail.com>
2021-03-20 13:00:41 -0600

    restableciendo

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.post('/signin', isNotLoggedIn,(req,res,next)=>{
     })(req,res,next);
 });
 
-router.get('/home', (req, res) => {
+router.get('/home',isLoggedIn, (req, res) => {
     res.render('home');
 });
 
