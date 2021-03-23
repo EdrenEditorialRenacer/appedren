d298084bb70eab235fb66e089d8e806bdc99a778
Dhux93 <mario_mario93@hotmail.com>
2021-03-23 13:20:01 -0600

    consulta sql cambiada

--- routes/premium.js
+++ routes/premium.js
@@ -24,61 +24,61 @@ router.get('/all',isLoggedIn,authRole('user_premium'), async (req, res) => {
 
 
 router.get('/libros',isLoggedIn,authRole('user_premium'), async (req, res) => {
-    const libros = await pool.query(`SELECT a.id, a.titulo,u.autor,p.portada,d.documento FROM archiveros a 
+    const libros = await pool.query(`SELECT a.id, a.titulo,u.autor,u.facebook,u.whattsap,u.email,p.portada,d.documento FROM archiveros a 
     INNER JOIN autores u ON a.idautor = u.id
     INNER JOIN portadas p ON p.idarchivero = a.id
     INNER JOIN documentos d ON d.idarchivero = a.id
-    WHERE a.modulos = 'libros' AND a.roles = 'premium' OR a.roles = 'free'`);
+    WHERE a.modulos = 'libros'`);
     console.log(libros);
     res.render('premium/libros',{libros});
 });
 
 router.get('/revistas',isLoggedIn,authRole('user_premium'), async (req, res) => {
-    const revistas = await pool.query(`SELECT a.id, a.titulo,u.autor,p.portada,d.documento FROM archiveros a 
+    const revistas = await pool.query(`SELECT a.id, a.titulo,u.autor,u.facebook,u.whattsap,u.email,p.portada,d.documento FROM archiveros a 
     INNER JOIN autores u ON a.idautor = u.id
     INNER JOIN portadas p ON p.idarchivero = a.id
     INNER JOIN documentos d ON d.idarchivero = a.id
-    WHERE a.modulos = 'revistas' AND a.roles = 'premium' OR a.roles = 'free'`);
+    WHERE a.modulos = 'revistas' `);
     console.log(revistas);
     res.render('premium/revistas',{revistas});
 });
 
 router.get('/comics', isLoggedIn,authRole('user_premium'),async (req, res) => {
-    const comics = await pool.query(`SELECT a.id, a.titulo,u.autor,p.portada,d.documento FROM archiveros a 
+    const comics = await pool.query(`SELECT a.id, a.titulo,u.autor,u.facebook,u.whattsap,u.email,p.portada,d.documento FROM archiveros a 
     INNER JOIN autores u ON a.idautor = u.id
     INNER JOIN portadas p ON p.idarchivero = a.id
     INNER JOIN documentos d ON d.idarchivero = a.id
-    WHERE a.modulos = 'comics' AND a.roles = 'premium' OR a.roles = 'free'`);
+    WHERE a.modulos = 'comics' `);
     console.log(comics);
     res.render('premium/comics',{comics});
 });
 
 router.get('/musica', isLoggedIn,authRole('user_premium'),async (req, res) => {
-    const musica = await pool.query(`SELECT a.id, a.titulo,u.autor,p.portada,d.documento FROM archiveros a 
+    const musica = await pool.query(`SELECT a.id, a.titulo,u.autor,u.facebook,u.whattsap,u.email,p.portada,d.documento FROM archiveros a 
     INNER JOIN autores u ON a.idautor = u.id
     INNER JOIN portadas p ON p.idarchivero = a.id
     INNER JOIN documentos d ON d.idarchivero = a.id
-    WHERE a.modulos = 'musica' AND a.roles = 'premium' OR a.roles = 'free'`);
+    WHERE a.modulos = 'musica'`);
     console.log(musica);
     res.render('premium/musica',{musica});
 });
 
 router.get('/peliculas',isLoggedIn,authRole('user_premium'), async (req, res) => {
-    const peliculas = await pool.query(`SELECT a.id, a.titulo,u.autor,p.portada,d.documento FROM archiveros a 
+    const peliculas = await pool.query(`SELECT a.id, a.titulo,u.autor,u.facebook,u.whattsap,u.email,p.portada,d.documento FROM archiveros a 
     INNER JOIN autores u ON a.idautor = u.id
     INNER JOIN portadas p ON p.idarchivero = a.id
     INNER JOIN documentos d ON d.idarchivero = a.id
-    WHERE a.modulos = 'peliculas' AND a.roles = 'premium' OR a.roles = 'free'`);
+    WHERE a.modulos = 'peliculas' `);
     console.log(peliculas);
     res.render('premium/peliculas',{peliculas});
 });
 
 router.get('/videos',isLoggedIn,authRole('user_premium'), async (req, res) => {
-    const videos = await pool.query(`SELECT a.id, a.titulo,u.autor,p.portada,d.documento FROM archiveros a 
+    const videos = await pool.query(`SELECT a.id, a.titulo,u.autor,u.facebook,u.whattsap,u.email,p.portada,d.documento FROM archiveros a 
     INNER JOIN autores u ON a.idautor = u.id
     INNER JOIN portadas p ON p.idarchivero = a.id
     INNER JOIN documentos d ON d.idarchivero = a.id
-    WHERE a.modulos = 'videos' AND a.roles = 'premium' OR a.roles = 'free'`);
+    WHERE a.modulos = 'videos' `);
     console.log(videos);
     res.render('premium/videos',{videos});
 });
