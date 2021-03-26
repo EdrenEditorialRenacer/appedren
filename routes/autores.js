d7f4cc25310e1bdf61941a96441ef2ed0087172e
Dhux93 <mario_mario93@hotmail.com>
2021-03-26 15:22:00 -0600

    noveno intento autores

--- routes/autores.js
+++ routes/autores.js
@@ -1,57 +1,65 @@
 const express = require('express');
 const pool = require('../database');
 const router = express.Router();
-const { isLoggedIn, authRole } = require('../lib/auth');
+const {isLoggedIn,authRole} =require('../lib/auth');
 
-router.get('/add', (req, res) => {
+router.get('/add', (req,res) => {
     res.render('autores/add');
 });
 
-router.post('/add', isLoggedIn, authRole('admin'), async (req, res) => {
-    const { autor, facebook, whattsap, email } = req.body;
-    const newAutor = {
-        autor: (String) = autor,
-        facebook: (String || null) = facebook,
-        whattsap: (String || null) = whattsap,
-        email: (String || null) = email
-    }
-    console.log(newAutor);
-    await pool.query('INSERT INTO autores set ?', [newAutor]);
-    req.flash('success', "Agregado con Exito");
-    res.redirect('/autores')
-});
-
-router.get('/', isLoggedIn, authRole('admin'), async (req, res) => {
-    const autores = await pool.query('SELECT * FROM autores');
-    console.log(autores);
-    res.render('autores/list', { autores });
-});
-
-
-router.get('/delete/:id', isLoggedIn, authRole('admin'), async (req, res) => {
-    const { id } = req.params;
-    await pool.query('DELETE FROM autores WHERE id = ?', [id]);
-    req.flash('success', "Eliminado con Exito");
-    res.redirect('/autores');
-});
-
-router.get('/edit/:id', isLoggedIn, authRole('admin'), async (req, res) => {
-    const { id } = req.params;
-    const autores = await pool.query('SELECT * FROM autores WHERE id=?', [id]);
-    res.render('autores/edit', { autor: autores[0] });
-});
-
-router.post('/edit/:id', isLoggedIn, authRole('admin'), async (req, res) => {
-    const { id } = req.params;
-    const { autor, facebook, whattsap, email } = req.body
+router.post('/add',isLoggedIn,authRole('admin'), async (req,res)=>{
+    const {autor,facebook,whattsap,email } = req.body;
     const newAutor = {
         autor,
         facebook,
         whattsap,
         email
     }
-    await pool.query('UPDATE autores set ? WHERE id = ?', [newAutor, id]);
-    req.flash('success', "Editado con Exito!")
+    console.log(newAutor);
+    if(!(newAutor.facebook == null || newAutor.whattsap == null || newAutor.email == null)){
+         await pool.query('INSERT INTO autores set ?',[newAutor]);
+         req.flash('success',"Agregado con Exito");
+    res.redirect('/autores')
+    }else{
+        await pool.query('INSERT INTO autores set ?',[newAutor]);
+        req.flash('success',"Agregado con Exito");
+   res.redirect('/autores');
+    }
+   
+    
+});
+
+router.get('/',isLoggedIn,authRole('admin'), async (req,res)=>{
+    const autores = await pool.query('SELECT * FROM autores');
+    console.log(autores);
+    res.render('autores/list', { autores });
+});
+
+
+router.get('/delete/:id',isLoggedIn,authRole('admin'), async (req,res)=>{
+    const {id} = req.params;
+    await pool.query('DELETE FROM autores WHERE id = ?',[id]);
+    req.flash('success',"Eliminado con Exito");
+    res.redirect('/autores');
+});
+
+router.get('/edit/:id',isLoggedIn,authRole('admin'),async (req,res)=>{
+    const { id } = req.params;
+     const autores= await pool.query('SELECT * FROM autores WHERE id=?',[id]);
+    res.render('autores/edit', {autor: autores[0]});
+});
+
+router.post('/edit/:id',isLoggedIn,authRole('admin'),async (req,res)=> {
+    const {id} =req.params;
+    const {autor,facebook,whattsap,email } = req.body
+    const newAutor = {
+        autor,
+        facebook,
+        whattsap,
+        email
+    }
+    await pool.query('UPDATE autores set ? WHERE id = ?',[newAutor,id]);
+    req.flash('success',"Editado con Exito!")
     res.redirect('/autores');
 })
 
